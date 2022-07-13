20a91ec0def718edc0d4829b20f174e92a6bde09
nirinaRakotoniary <nirinamercia@gmail.com>
2022-07-13 12:56:23 +0100

    second commit

--- script/script.js
+++ script/script.js
@@ -4,6 +4,16 @@ window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", window.scrollY > 50);
     
 });
+
 /* Date sur le copyright */
 let currentDate = new Date();
 document.getElementById("year").innerHTML = currentDate.getFullYear();
+
+/* Montrer les compétences */
+document.getElementById('lg').style.display = "none";
+document.getElementById('hb').style.display = "none";
+function showSkill(s, ns1, ns2){
+    document.getElementById(s).style.display = "block";
+    document.getElementById(ns1).style.display = "none";
+    document.getElementById(ns2).style.display = "none";
+}
